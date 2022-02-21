2e243f5dbe79c77d772f161e70181c72f428b505
Aidi <aidi.saifudin@gmail.com>
2022-02-21 17:36:18 +0800

    DONE

---
+++ VRAssignment2Copy/Assets/Scripts/PauseBtn.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class PauseBtn : MonoBehaviour
+{
+
+    
+   
+    public GameObject resumeBtn;
+    public GameObject pauseButton;
+    public AudioSource audio;
+    public AudioSource audioTwo;
+
+
+
+
+    public void PausedOnPress()
+    {
+        
+        
+        pauseButton.SetActive(false);
+        resumeBtn.SetActive(true);
+        Time.timeScale = 0;
+        audio.Stop();
+        audioTwo.Stop();
+        
+    }
+
+    public void ResumeOnPress()
+    {
+        
+        pauseButton.SetActive(true);
+        resumeBtn.SetActive(false);
+        Time.timeScale = 1;
+        audio.Play();
+        audioTwo.Play();
+    }
+}
